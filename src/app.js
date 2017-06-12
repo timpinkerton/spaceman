6323afedca437ad7a2834ebafacc1f6507e9a910
Tim Pinkerton <timpinkerton@mail.com>
2017-06-11 22:27:35 -0400

    Added templates and bootstrap css

--- src/app.js
+++ src/app.js
@@ -6,12 +6,19 @@ const express = require('express'),
       //This is to make use of the mock data
         posts = require('./mock/posts.json');
 
+//Common pattern in JS to turn and Object into an array
+//Object.keys generates an array of keys from the post object
+//An array has .map method.  Allows to iterate through each item in the array and create a new array
+//In this case the callback to the .map method is a function.
+var postsLists = Object.keys(posts).map(function(value) {
+  return posts[value]})
+
 //Assigning express to the new variable "app"
 const app = express();
 
 //app.set defines different settings in express
 //This tells express how to render our engine
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.set('views', __dirname + '/templates');
 
 
@@ -31,7 +38,8 @@ app.get('/blog/:title?', function(req, res){
   if (title === undefined) {
     //response status of 503 indicates service is unavailable
     res.status(503);
-    res.send("This page is under construction!")
+    res.render('post', {posts: postsLists})    
+//    res.send("This page is under construction!")
   } else {
     
     //if the post does not exist, it's defined as an empty object {}
